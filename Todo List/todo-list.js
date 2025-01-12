bf4f10bdbc00ee02cc9e6b2c8b8d16b72136470f
Ayushpathak18 <ayushpathak7777@gmail.com>
2025-01-12 23:46:15 +0530

    update

--- Todo List/todo-list.js
+++ Todo List/todo-list.js
@@ -1,25 +1,40 @@
-const todolist=[];
+const todolist = [{
+    name: 'make dinner',
+    duedate: '2022-12-22'
+  }, {
+    name: 'wash dishes',
+    duedate: '2022-12-22'
+  }];
 renderTodoList();
 function addTodo(){
     const inputElement=document.querySelector('.js-name-input');
     const name=inputElement.value;
-    todolist.push(name);
+
+    const dateinputElement=document.querySelector('.js-due-date-input');
+    const duedate=dateinputElement.value;
+
+    todolist.push({name,duedate});
     
-    inputElement.value=' '
+    inputElement.value='';
     renderTodoList();
 }
 function renderTodoList(){
     let todolistHTML=' ';
     for(let i=0;i<todolist.length;i++){
         const todo=todolist[i];
+        //const name=todoObject.name;
+        //const duedate=todoObject.duedate;
+        const{name,duedate}=todo;
         const html=`
-        <p>
-        ${todo}
-        <button>Delete</button>
-        </p>`;
+        <div>${name}</div>
+        <div>${duedate}</div>
+        <button onclick="
+        todolist.splice(${i},1);
+        renderTodoList();
+        ">Delete</button>
+        `;
         todolistHTML+=html;
     }
-    console.log(todolistHTML);
     document.querySelector('.js-todo-list').innerHTML=todolistHTML;
     }
    
