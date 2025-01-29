ff06e79cae24a76496a0844bccecb7f9bf0d087a
Ayushpathak18 <ayushpathak7777@gmail.com>
2025-01-29 23:41:06 +0530

    commit

--- javascriptroject3/script.js
+++ javascriptroject3/script.js
@@ -1,17 +1,17 @@
 var tl=gsap.timeline()
-tl.to("page 1",{
-    y:"100vh"
+tl.to("#page1",{
+    y:"100vh",
     scale:0.6,
     duration:0
 })
- tl.from("#page1",{
+ tl.to("#page1",{
     y:"30vh",
     duration:1,
     delay:1,
 })
-tl.from("#page1",{
+tl.to("#page1",{
     y:"0vh",
     rotate:360,
     scale:1,
-    duration:0,
+    duration:0.8
 })
